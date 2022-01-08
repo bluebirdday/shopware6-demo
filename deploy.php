d96f08f07002a633a3afaea5482529edfa7c8a54
Martin Dubbelman <martindubbelman@bluebirdday.nl>
2022-01-08 14:55:48 +0100

    Run meta data command in deployment

--- deploy.php
+++ deploy.php
@@ -105,9 +105,9 @@ $configuration->addBuildCommand(new BuildAdministration());
 $configuration->addBuildCommand(new BuildStorefront());
 $configuration->addBuildCommand(new ThemeCompile());
 $configuration->addBuildCommand(new Command('{{bin/php}} bin/console assets:install'));
-// $configuration->addBuildCommand(new Command('{{bin/php}} bin/console deployment:metadata:create'));
 
 
+$configuration->addDeployCommand(new DeployCommand('{{bin/php}} bin/console deployment:metadata:create'));
 $configuration->addDeployCommand(new DeployCommand('{{bin/php}} bin/console database:migrate --all'));
 $configuration->addDeployCommand(new DeployCommand('{{bin/php}} bin/console plugin:refresh'));
 $configuration->addDeployCommand(new CacheClear());
